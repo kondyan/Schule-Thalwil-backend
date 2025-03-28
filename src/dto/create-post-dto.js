b6a94d6a49ff62b232a6ea925d4df51451f533d5
kondyan <arsenij.kondratenko@gmail.com>
2025-03-28 14:09:25 +0100

    15-commit

--- src/dto/create-post-dto.js
+++ src/dto/create-post-dto.js
@@ -2,12 +2,12 @@ const Joi = require("joi");
 
 const createPostDto = Joi.object({
   title: Joi.string()
-    .pattern(/^[A-Za-zöäü0-9 ().,;<>""«»?-!]+$/)
+    .pattern(/^[A-Za-zöäü0-9 -().,;<>""«»?!]+$/)
     .min(2)
     .max(27),
   imageUrl: Joi.string(),
   content: Joi.string()
-    .pattern(/^[A-Za-zöäü0-9 ().,;<>""«»?-!]+$/)
+    .pattern(/^[A-Za-zöäü0-9 -().,;<>""«»?!]+$/)
     .min(40)
     .max(1000),
   isDraft: Joi.bool(),
