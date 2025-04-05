6b1c3f9fe1f40b041d3a1675a0669c164139aa42
kondyan <arsenij.kondratenko@gmail.com>
2025-04-05 23:22:39 +0200

    41-commit

--- src/dto/create-post-dto.js
+++ src/dto/create-post-dto.js
@@ -2,12 +2,12 @@ const Joi = require("joi");
 
 const createPostDto = Joi.object({
   title: Joi.string()
-    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»@#$%^&*“”‘’()\[\]-\s+]+$/)
+    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»""''@#$%^&*“”‘’()\[\]-\s+]+$/)
     .min(2)
     .max(27),
   imageUrl: Joi.string(),
   content: Joi.string()
-    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»@#$%^&*“”‘’()\[\]-\s+]+$/)
+    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»""''@#$%^&*“”‘’()\[\]-\s+]+$/)
     .min(40)
     .max(1500),
   isDraft: Joi.bool(),
--- src/dto/create-tutorial-dto.js
+++ src/dto/create-tutorial-dto.js
@@ -2,12 +2,12 @@ const Joi = require("joi");
 
 const createTutorialDto = Joi.object({
   title: Joi.string()
-    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»@#$%^&*“”‘’()\[\]-\s+]+$/)
+    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»""''@#$%^&*“”‘’()\[\]-\s+]+$/)
     .min(2)
     .max(27),
   videoUrl: Joi.string(),
   description: Joi.string()
-    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»@#$%^&*“”‘’()\[\]-\s+]+$/)
+    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»""''@#$%^&*“”‘’()\[\]-\s+]+$/)
     .min(10)
     .max(200),
   isDraft: Joi.bool(),
