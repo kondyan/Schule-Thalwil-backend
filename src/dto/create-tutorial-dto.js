f5cf39bfb81187261fd265f0a75d72040b5d6a22
kondyan <arsenij.kondratenko@gmail.com>
2025-04-10 21:58:56 +0200

    51-commit

--- src/dto/create-tutorial-dto.js
+++ src/dto/create-tutorial-dto.js
@@ -3,12 +3,12 @@ const Joi = require("joi");
 const createTutorialDto = Joi.object({
   category: Joi.required(),
   title: Joi.string()
-    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»""''@#$%^&*“”‘’()\[\]-\s+]+$/)
+    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»-""''@#$%^&*“”‘’()\[\]-\s+]+$/)
     .min(2)
     .max(27),
   videoUrl: Joi.string(),
   description: Joi.string()
-    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»""''@#$%^&*“”‘’()\[\]-\s+]+$/)
+    .pattern(/^[A-Za-zäöüÄÖÜß0-9\s.,:;?!–«»-""''@#$%^&*“”‘’()\[\]-\s+]+$/)
     .min(10)
     .max(200),
   isDraft: Joi.bool(),
