1f7ea2a47c4250c3f1180bedd765e7b894b3751b
kondyan <arsenij.kondratenko@gmail.com>
2025-04-11 00:42:16 +0200

    57-commit

--- src/dto/update-user-dto.js
+++ src/dto/update-user-dto.js
@@ -7,7 +7,9 @@ const updateUserDto = Joi.object({
 
   secondName: Joi.string().alphanum().min(3).max(20),
 
-  password: Joi.string().allow().min(8),
+  password: Joi.when("password", { is: Joi.exist(), then: Joi.string().min(8) })
+    .string()
+    .min(8),
 
   role: Joi.array().items(Joi.string().valid("writer", "moderator", "admin")),
   // .required(),
