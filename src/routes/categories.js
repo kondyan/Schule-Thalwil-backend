fcf26bd6b6f5951cbce4f3802d1893afd56da446
kondyan <arsenij.kondratenko@gmail.com>
2025-04-10 15:46:50 +0200

    48-commit

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,6 +3,7 @@ const ctrl = require("../controllers/categories");
 const { validateBody } = require("../middleware/validate-body");
 const { createCategoryDto } = require("../dto/create-category-dto");
 const { authenticate } = require("../middleware/authenticate");
+const { access } = require("../middleware/access");
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.get("/", ctrl.getCategories);
 router.post(
   "/",
   authenticate,
+  access("moderator"),
   validateBody(createCategoryDto),
   ctrl.createCategory
 );
@@ -18,10 +20,11 @@ router.post(
 router.patch(
   "/:id",
   authenticate,
+  access("moderator"),
   validateBody(createCategoryDto),
   ctrl.updateCategory
 );
 
-router.delete("/:id", authenticate, ctrl.deleteCategory);
+router.delete("/:id", authenticate, access("moderator"), ctrl.deleteCategory);
 
 module.exports = router;
